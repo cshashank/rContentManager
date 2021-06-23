1d3175e80499c22a58d8a4a35144417b117112fb
cshashank <shashank@timbergrove.com>
2021-06-22 21:52:07 -0400

    basic poem loop working

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
 import logo from './logo.svg';
+import React, { Component } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
 import MarathiPoems from './components/MarathiPoems';
 import EnglishPoems from './components/EnglishPoems';
+import Navbar from './components/Navbar'
 
 function App() {
-  return (
-    <div className="App">
-      SKC Test 
-      <MarathiPoems />
-      <EnglishPoems />
+  const Home = () => (
+    <div>
+      <h2>Home</h2>
     </div>
+  );
+
+  return (
+    <main>
+      <Navbar></Navbar>
+      <Switch>
+        <Route path="/" component={Home} exact />
+        <Route path="/marathi" component={MarathiPoems} />
+        <Route path="/english" component={EnglishPoems} />
+      </Switch>
+    </main>  
   );
 }
 
 export default App;
+  
--- src/components/MarathiPoems.js
+++ src/components/MarathiPoems.js
@@ -1,9 +1,35 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 export default function MarathiPoems() {
+    localStorage.setItem('poemURL', 'marathi1');
+    const [items, setItems] = useState([{}])
+    useEffect(() => {
+        const tempUrl = localStorage.getItem('poemURL');
+        fetch('data/marathiDb.json'
+            , {
+                "headers": {
+                    "ContentType": "application/json",
+                    'Accept': 'application/json'
+                }
+            })
+            .then(response => {
+                console.log(response)
+                return response.json()
+            })
+            .then(data =>{
+                setItems(data)
+                console.log(' setting data  '+ data)
+            });
+    },[]);
+
     return (
         <div>
-            Marathi poems
+            Marathi poems {items[0].name}
+            {items.map((item,index) => (
+                <div key={index}>
+                    <h3>{item.name} </h3>
+                </div>
+            ))}
         </div>
-    )
+    );
 }
