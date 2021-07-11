df8ea9870114171ee3c86459ca84b20c4b778339
cshashank <chitnis.shashank@gmail.com>
2021-07-11 14:12:08 -0400

    Created material ui for marathi poems

--- src/components/MarathiPoems.js
+++ src/components/MarathiPoems.js
@@ -1,6 +1,36 @@
 import React, { useState, useEffect } from 'react'
+import { withStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+import AppBar from '@material-ui/core/AppBar';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Typography from '@material-ui/core/Typography';
 
-export default function MarathiPoems() {
+const styles = theme => ({
+    card: {
+        width: 200,
+        height: 70,
+        textAlign: 'center',
+        background: 'lightblue',
+        border: 'black',
+        margin: 10
+    },
+    cardActions: {
+        justifyContent: 'center'
+    },
+    root: {
+        flexGrow: 1,
+        backgroundColor: theme.palette.background.paper
+    },
+    tabContent: {
+        padding: theme.spacing.unit * 2
+    }
+});
+
+const MarathiPoems = withStyles(styles)(({ classes }) => {
     localStorage.setItem('poemURL', 'marathi1');
     const [items, setItems] = useState([{}])
     useEffect(() => {
@@ -16,20 +46,46 @@ export default function MarathiPoems() {
                 console.log(response)
                 return response.json()
             })
-            .then(data =>{
+            .then(data => {
                 setItems(data)
-                console.log(' setting data  '+ data)
+                console.log(' setting data  ' + data)
             });
-    },[]);
+    }, []);
+
+    const [value, setValue] = useState(0);
+
+    const onChange = (e, value) => {
+        setValue(value);
+    };
 
     return (
         <div>
             Marathi poems links {items[0].name}
-            {items.map((item,index) => (
-                <div key={index}>
-                    <h3>{item.name} </h3>
-                </div>
-            ))}
+            <div className={classes.root}>
+                <AppBar position="static">
+                    <Tabs value={value} onChange={onChange}>
+                        <Tab label="Poems" />
+                        <Tab label="Articles" />
+                    </Tabs>
+                </AppBar>
+                {value === 0 && (
+                    <Typography component="div" className={classes.tabContent}>
+                        {items.map((item, index) => (
+                            <Card className={classes.card} key={index}>
+                                <CardContent>
+                                    <Typography variant="h5" color="primary">{item.name}</Typography>
+                                </CardContent>
+                            </Card>
+                        ))}
+                    </Typography>
+                )}
+                {value === 1 && (
+                    <Typography component="div" className={classes.tabContent}>
+                        Item Two
+                    </Typography>
+                )}
+            </div>
         </div>
     );
-}
+})
+export default MarathiPoems;
